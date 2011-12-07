642750d62a5c7ace0c4331f1e779cf803949e6a8
Tobias Lidskog <tobias.lidskog@cybercom.com>
2011-12-07 17:59:02 +0100

    Script to create chef repos
    Script that creates a local "chef-repo" folder, based on the opscode
    chef-repo template. A git repository is created, and a few custom
    settings are added.

---
+++ create-chef-repo.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if [ -d chef-repo ]; then
+ echo "chef-repo folder already exists, exiting!"
+ exit 1
+fi
+
+echo "cloning OpsCode repo template"
+git clone git://github.com/opscode/chef-repo.git
+
+cd chef-repo
+
+echo "removing git folders from template"
+rm -rf .git
+
+echo "initializing new git repo"
+git init
+git add .
+git commit -m "initial commit"
+
+echo "adding site-cookbooks, for custom cookbooks"
+mkdir site-cookbooks
+echo "Add custom cookbooks here" > site-cookbooks/README
+git add site-cookbooks
+git commit -m "adding site-cookbooks folder"
+
+echo "adding .chef/knife.rb, for custom knife settings"
+mkdir .chef
+cat > .chef/knife.rb <<END_OF_FILE
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+END_OF_FILE
+git add .chef
+git commit -m "adding .chef folder with knife.rb settings for cookbook paths"
+
+
+echo "You now have a clean Chef repo of your own. Go on and use 'knife cookbook site install COOKBOOK_NAME' to install community cookbooks in a controlled way. Use git submodules to add 3rd party cookbooks"
+echo "remaining work is to add scripts for adding 3rd party cookbooks, and more"
+
